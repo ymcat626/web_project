8d97b7aaf46938256bde778a62042cc242609eff
ymcat <meattimes@live.cn>
2018-07-27 22:54:59 +0800

    flask demo

---
+++ web013/message.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+import time
+
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+)
+
+# 先要初始化一个 Flask 的实例
+app = Flask(__name__)
+
+# message_list 用来存储所有的 message
+message_list = []
+
+
+def log(*args, **kwargs):
+    format = '%Y/%m/%d %H:%M:%S'
+    value = time.localtime(time.time())
+    dt = time.strftime(format, value)
+
+    # 保存日志到log.txt
+    with open('log.txt', 'w', encoding='utf-8') as f:
+        print(dt, *args, file=f, **kwargs)
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return 'hello, world'
+
+
+@app.route('/message')
+def message_view():
+    log('请求方法', request.method)
+    log('request, query 参数', request.args)
+    return render_template('message_index.html', messages=message_list)
+
+
+if __name__ == '__main__':
+    config = dict(
+        host='0.0.0.0',
+        port='2000',
+        debug=True,
+    )
+    app.run(**config)
