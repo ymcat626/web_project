acd42ab0f768058cd572d4c6d11ec3bb16e682dd
ymcat <meattimes@live.cn>
2018-08-22 23:31:10 +0800

    add remove done funtion

--- js/js007/todo.js
+++ js/js007/todo.js
@@ -7,6 +7,7 @@ var e = function (selector) {
 }
 
 var addButton = e('#id-button-add')
+// 给 addButton 加事件绑定
 addButton.addEventListener('click', function () {
     // 获取 todo 元素
     var todo = e('#id-input-content')
@@ -16,23 +17,47 @@ addButton.addEventListener('click', function () {
 
 })
 
+// 用来生成 todo 的模版方法
 var todoTemplate = function (todo) {
     var t = `
-        <div>
-            <span>${ todo }</span>
-            <button>delete</button>
-            <button>done</button>
+        <div class="todo-cell">
+            <span contenteditable="true">${ todo }</span>
+            <button class="todo-delete">delete</button>
+            <button class="todo-done">done</button>
         </div>
     `
     return t
 }
 
+// 向节点中插入 todo
 var insertTodo = function (todo) {
-    var todoList = e('.todo-list')
+    var todoList = e('#id-div-container')
     t = todoTemplate(todo)
-    todoList.insertAdjacentHTML('beforeEnd', t)
+    todoList.insertAdjacentHTML('beforeend', t)
 }
 
-var deleteButton = e('')
+var container = e('#id-div-container')
+
+container.addEventListener('click', function (event) {
+    log('event', event)
+    var target = event.target
+    log('target', target)
+    if (target.classList.contains('todo-delete')) {
+        log('delete')
+        target.parentElement.remove()
+    }else if(target.classList.contains('todo-done')) {
+        var todoDiv = target.parentElement
+        toggleClass(todoDiv, 'done')
+    }
+})
+
+// 此函数用来切换一个元素的某个 class
+var toggleClass = function (element, className) {
+    if (element.classList.contains(className)) {
+        element.classList.remove(className)
+    }else {
+        element.classList.add(className)
+    }
+}
 
 
