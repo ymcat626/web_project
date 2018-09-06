0e047f67776fedc0dd23b20316da1cd111faf7b1
ymcat <meattimes@live.cn>
2018-09-06 11:21:46 +0800

    update nav for detail.html

--- web018_release/models/__init__.py
+++ web018_release/models/__init__.py
@@ -31,6 +31,7 @@ class Model(object):
     user = User()
     user.db_path() 返回 User.txt
     """
+
     @classmethod
     def db_path(cls):
         """
@@ -187,3 +188,17 @@ class Model(object):
         l = [m.__dict__ for m in models]
         path = self.db_path()
         save(l, path)
+
+    def time(self):
+        t = self.ct
+        delta = int(time.time() - t)
+        if delta < 60:
+            return '1分钟前'
+        if delta < 3600:
+            return '%s分钟前' % (delta // 60)
+        if delta < 86400:
+            return '%s小时前' % (delta // 3600)
+        if delta < 604800:
+            return '%s天前' % (delta // 86400)
+        dt = datetime.fromtimestamp(t)
+        return '%s年%s月%s日' % (dt.year, dt.month, dt.day)
--- web018_release/models/reply.py
+++ web018_release/models/reply.py
@@ -18,16 +18,3 @@ class Reply(Model):
         u = User.find(self.user_id)
         return u
 
-    def time(self):
-        t = self.ct
-        delta = int(time.time() - t)
-        if delta < 60:
-            return '1分钟前'
-        if delta < 3600:
-            return '%s分钟前' % (delta // 60)
-        if delta < 86400:
-            return '%s小时前' % (delta // 3600)
-        if delta < 604800:
-            return '%s天前' % (delta // 86400)
-        dt = datetime.fromtimestamp(t)
-        return '%s年%s月%s日' % (dt.year, dt.month, dt.day)
