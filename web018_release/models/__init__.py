2b89ccaa200dae6d7d471942106a1668e3f94de1
ymcat <meattimes@live.cn>
2018-09-06 23:32:39 +0800

    update timeformat

--- web018_release/models/__init__.py
+++ web018_release/models/__init__.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import time
 from ..utils import log
--- web019/models/board.py
+++ web019/models/board.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 from models import Model
 
@@ -8,3 +9,17 @@ class Board(Model):
         self.title = form.get('title', '')
         self.ct = int(time.time())
         self.ut = self.ct
+
+    def time(self):
+        t = self.ct
+        delta = int(time.time() - t)
+        if delta < 60:
+            return '1分钟前'
+        if delta < 3600:
+            return '%s分钟前' % (delta // 60)
+        if delta < 86400:
+            return '%s小时前' % (delta // 3600)
+        if delta < 604800:
+            return '%s天前' % (delta // 86400)
+        dt = datetime.fromtimestamp(t)
+        return '%s年%s月%s日' % (dt.year, dt.month, dt.day)
